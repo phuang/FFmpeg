901248a373cbbe7af68fb92faf3be7d4f679150d
Ted Meyer <tmathmeyer@chromium.org>
2024-04-08 14:25:35 -0700

    Remove scripts which now exist in chromium
    Bug: 330573128
    Change-Id: I9d24c978806891429a05e1716e287c8e2d938f74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/third_party/ffmpeg/+/5436037
    Reviewed-by: Frank Liberato <liberato@chromium.org>

--- chromium/scripts/generate_gn_unittest_wrapper.py
+++ chromium/scripts/generate_gn_unittest_wrapper.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env vpython3
+# Copyright 2024 The Chromium Authors.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
 '''Wrapper script to appease the PRESUBMIT.py vpython dark spirits.'''
 
 import os
